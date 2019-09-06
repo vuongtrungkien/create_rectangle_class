90de86e54d5abc1b89a8a2371dcd8959cc18e6ac
vuongtrungkien <nobitaa6@gmail.com>
2019-09-06 10:08:00 +0700

    file class Rectangle

---
+++ Rectangle.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Rectangle
+{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height)
+    {
+        $this->height = $height;
+        $this->width = $width;
+    }
+
+    public function getArea()
+    {
+        return ($this->width * $this->height);
+    }
+
+    public function getPerimeter()
+    {
+        return (($this->height + $this->width)*2);
+    }
+
+    public function display () {
+        return "Rectangle { " . "width = " . $this->width. "height = " .$this->height."}";
+    }
+
+
+}
